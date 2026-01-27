3400b4f4aa21f2e27d4c5e7ba6ad9b810f7ecd81
OtavioLhamas <otavio.m.lhamas@gmail.com>
2026-01-26 22:35:45 -0300

    feat(dial): add semver to csharp and xml groups

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -70,27 +70,30 @@ return {
             cyclic = true,
         })
 
+        local and_or = augend.constant.new({
+            elements = { "and", "or" },
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true, -- "or" is incremented into "and".
+        })
+
         table.insert(opts.groups.default, numeros_ordinarios)
         table.insert(opts.groups.default, numeros_ordinarias)
         table.insert(opts.groups.default, dias_semana)
         table.insert(opts.groups.default, meses)
-        table.insert(
-            opts.groups.default,
-            augend.constant.new({
-                elements = { "and", "or" },
-                word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
-                cyclic = true, -- "or" is incremented into "and".
-            })
-        )
         table.insert(opts.groups.default, augend.date.alias["%Y-%m-%d"])
         table.insert(opts.groups.default, augend.date.alias["%d/%m/%Y"])
 
+        opts.dials_by_ft.cs = "cs"
         opts.groups.cs = {
             augend.constant.new({ elements = { "var", "const" } }),
-            augend.constant.new({ elements = { "public", "private" } }),
+            augend.constant.new({ elements = { "public", "private", "protected" } }),
+            augend.semver.alias.semver,
         }
 
-        opts.dials_by_ft.cs = "cs"
+        opts.dials_by_ft.xml = "xml"
+        opts.groups.xml = {
+            augend.semver.alias.semver,
+        }
 
         return opts
     end,
