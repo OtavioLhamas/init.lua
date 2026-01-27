087dc70e9083fc1b9d5abd1d3e69adbfef4babbd
OtavioLhamas <otavio.m.lhamas@gmail.com>
2026-01-26 22:26:11 -0300

    feat: consolidate float windows border in opt.winborder

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,6 @@
 vim.g.deprecation_warnings = true
 vim.g.autoformat = false
 vim.g.snacks_animate = false
-vim.g.lazyvim_prettier_needs_config = true
 
 if LazyVim.is_win() then
     -- Try to get PowerShell version, redirecting stderr to null
@@ -20,19 +19,12 @@ else
 end
 
 local opt = vim.opt
-local b = vim.b
-
-b.autoformat = false
 
 opt.clipboard = ""
-opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
-opt.conceallevel = 0 -- Text is shown normally
--- opt.foldcolumn = "0" -- disable foldcolumn
--- opt.foldenable = true
--- opt.foldlevel = 99 -- using ufo provider needs a large value
--- opt.foldlevelstart = 99
 opt.hlsearch = true -- highlight all matches on previous search pattern
 opt.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
+opt.signcolumn = "yes"
 opt.softtabstop = 4 -- number of spaces tabs counts for while performing editing operations
 opt.tabstop = 4 -- number of spaces tabs count for
+opt.winborder = "solid" -- default window border (affects blink windows, snacks picker and explorer)
 opt.wrap = true -- enable line wrap
