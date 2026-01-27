9a272fc869a8192dc7294076446623a8f507ab81
OtavioLhamas <otavio.m.lhamas@gmail.com>
2026-01-26 22:38:17 -0300

    feat(mason): add LSPs to ensure_installed that are not covered by LazyExtras

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,30 @@
+return {
+    "mason.nvim",
+    opts = {
+        ensure_installed = {
+
+            --#region LSP
+            -- TODO: compare css lsp
+            "css-lsp",
+            -- "css-variables-language-server",
+            -- "cssmodules-language-server",
+            -- "some-sass-language-server",
+
+            "fish-lsp",
+
+            -- "groovy-language-server",
+
+            "html-lsp",
+
+            "jinja-lsp",
+
+            "powershell-editor-services",
+
+            "bash-language-server",
+
+            "lemminx", -- XML
+
+            --#endregion
+        },
+    },
+}
