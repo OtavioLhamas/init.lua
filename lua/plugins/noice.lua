1a56933f2502030f82d2f3760ea2cb50b06ba60c
OtavioLhamas <otavio.m.lhamas@gmail.com>
2026-01-26 22:38:47 -0300

    chore(noice): update opts

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,38 +1,35 @@
 return {
     "noice.nvim",
-    opts = function(_, opts)
-        opts.cmdline = {
+    --TODO: come back here to config inc-rename.nvim once it is enabled
+    opts = {
+        cmdline = {
             view = "cmdline",
-        }
-
-        -- customize the inc-rename.nvim window
-        table.insert(opts.routes, {
-            filter = {
-                event = "cmdline",
-                kind = "IncRename",
-            },
-            opts = {
-                position = {
-                    col = 0,
-                    row = 2,
-                },
-                relative = "cursor",
-                size = {
-                    min_width = 20,
-                },
-            },
-            -- this allows me to keep IncRename relative to the cursor,
-            -- and a classic cmdline at the same time
-            view = "cmdline_popup",
-        })
-
-        opts.presets = LazyVim.merge(opts.presets, {
+        },
+        presets = {
             bottom_search = true,
-            command_palette = false,
+            command_pallete = false,
+            inc_rename = {
+                cmdline = {
+                    format = {
+                        IncRename = {
+                            opts = {
+                                position = {
+                                    col = 0,
+                                    row = -3,
+                                },
+                                relative = "cursor",
+                                size = {
+                                    min_width = 20,
+                                },
+                            },
+                            -- this allows me to keep IncRename relative to the cursor,
+                            -- and a classic cmdline at the same time
+                            view = "cmdline_popup",
+                        },
+                    },
+                },
+            },
             lsp_doc_border = true,
-            inc_rename = true,
-        })
-
-    end,
+        },
+    },
 }
-
