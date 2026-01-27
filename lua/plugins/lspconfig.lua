2dd1654de450768e391ddd6a0a6fc4e8a98dc278
OtavioLhamas <otavio.m.lhamas@gmail.com>
2026-01-26 22:36:47 -0300

    chore(lspconfig): update opts from function to table and use new lsp keys recipe

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,42 +1,38 @@
 return {
     "nvim-lspconfig",
-    opts = function(_, opts)
-        local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    opts = {
+        --#region LSP diagnostics
+        diagnostics = {
 
-        keys[#keys + 1] = { "gl", vim.diagnostic.open_float, desc = "Line Diagnosctics" }
-
-        -- configure lsp to use nvim-ufo folding
-        local lsp_capabilities = {}
-        if LazyVim.has("nvim-ufo") then
-            lsp_capabilities.textDocument = {
-                foldingRange = {
-                    dynamicRegistration = false,
-                    lineFoldingOnly = true,
-                },
-            }
-        end
-        opts.capabilities = LazyVim.merge(opts.capabilities, lsp_capabilities)
-
-        opts.codelens = LazyVim.merge(opts.codelens, { enabled = false })
-
-        opts.diagnostics = LazyVim.merge(opts.diagnostics, {
+            --#region Float window (<leader>gl)
             float = {
-                border = "rounded",
                 focusable = true,
                 header = "",
                 prefix = "",
                 severity_sort = true,
                 style = "minimal",
             },
+            --#endregion
+
+            --#region Virtual text
             virtual_text = {
                 spacing = 4,
                 source = "if_many",
                 severity = { min = vim.diagnostic.severity.WARN },
                 prefix = "icons",
             },
-        })
+            --#endregion
+        },
+        --#endregion
 
-        return opts
-    end,
+        --#region Server specific opts
+        servers = {
+            ["*"] = { -- all servers
+                keys = {
+                    { "gl", vim.diagnostic.open_float, desc = "Line Diagnosctics" },
+                },
+            },
+        },
+        --#endregion
+    },
 }
-
